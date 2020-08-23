7a82a6edbecc387556619fed8608b0b6297976a9
JessG9 <jessicagu999@gmail.com>
2020-08-23 18:12:04 -0400

    css

--- contact.js
+++
@@ -1,3 +0,0 @@
-<script language=”JavaScript”>
-
-var frmvalidator = new Validator(“contactform”); frmvalidator.addValidation(“name”,”req”,”Jessica”); frmvalidator.addValidation(“email”,”req”,”Please provide your email”); frmvalidator.addValidation(“email”,”email”, “jessicagu999@gmail.com”); </script>
